5e1b621bc67da26c9c20984b92bb84c9cb2072fd
Filipe Borges <f3l5p7@yahoo.com.br>
2023-11-12 17:30:47 +0100

    Fixed an error on the CO2 calculatoin using Peng-Robinson EOS.

--- ExampleFluidDensities.m
+++ ExampleFluidDensities.m
@@ -3,50 +3,40 @@
 % of methane and CO2 as a function of pressure at a given temperature.
 % Written by Filipe Borges in 2023
 
-%% Preparing the Workspace
-clear; 
-close all; 
-clc;
-
-%% Set Graphic Properties
-set(groot, 'DefaultTextInterpreter', 'latex');
-set(groot, 'DefaultTextFontSize', 14); 
-set(groot, 'DefaultTextFontName', 'Trebuchet MS');
-set(groot, 'DefaultLegendInterpreter', 'latex');
-set(groot, 'DefaultLegendFontSize', 14); 
-set(groot, 'DefaultLegendFontName', 'Trebuchet MS');
-set(groot, 'DefaultAxesTickLabelInterpreter', 'latex');
-set(groot, 'DefaultAxesFontSize', 12); 
-set(groot, 'DefaultAxesFontName', 'Trebuchet MS');
-
 %% Setup
 % Define the range of pressures and the temperature for the analysis.
-pressures = linspace(1e5, 3e7, 100); % Pressure range in Pascal
+pressures = linspace(1e5, 3e7, 500); % Pressure range in Pascal
 temperatureC = 35; % Temperature in Celsius
 
-methaneDensities = zeros(size(pressures));
-co2Densities = zeros(size(pressures));
+methaneDensitiesPR = zeros(size(pressures));
+co2DensitiesPR = zeros(size(pressures));
 methaneDensitiesIdealGas = zeros(size(pressures));
+co2DensitiesIdealGas = zeros(size(pressures));
 
 %% Compute Densities
 % Call the computeFluidDensity function for each pressure value for each fluid.
 for ii = 1:length(pressures)
-    methaneDensities(ii) = computeFluidDensity(pressures(ii), temperatureC, "methane");
-    co2Densities(ii) = computeFluidDensity(pressures(ii), temperatureC, "co2");
-    methaneDensitiesIdealGas(ii) = computeMethaneDensityIdeal(pressures(ii), temperatureC);
+    methaneDensitiesPR(ii) = computeDensityPengRobinson(pressures(ii), temperatureC, "ch4");
+    co2DensitiesPR(ii) = computeDensityPengRobinson(pressures(ii), temperatureC, "co2");
+    methaneDensitiesIdealGas(ii) = computeIdealGasDensity(pressures(ii), temperatureC,"ch4");
+    co2DensitiesIdealGas(ii) = computeIdealGasDensity(pressures(ii), temperatureC,"co2");
 end
 
 %% Plotting
 % Plot the densities as a function of pressure for both methane and CO2.
 
 figure;
-plot(pressures/1e5, methaneDensities, '-k', 'LineWidth', 2);
+set(gcf, 'Position', [100, 100, 1400, 600]);
+plot(pressures/1e5, methaneDensitiesPR, '-k', 'LineWidth', 2,'DisplayName','CH$_4$ - Peng-Robinson');
 hold on;
-plot(pressures/1e5, methaneDensitiesIdealGas, '-g', 'LineWidth', 2);
-plot(pressures/1e5, co2Densities, '-r', 'LineWidth', 2);
+box on;
+plot(pressures/1e5, methaneDensitiesIdealGas, '-g', 'LineWidth', 2,'DisplayName','CH$_4$ - Ideal gas');
+plot(pressures/1e5, co2DensitiesPR, '-r', 'LineWidth', 2,'DisplayName','CO$_2$ - Peng-Robinson');
+plot(pressures/1e5, co2DensitiesIdealGas, '-b', 'LineWidth', 2,'DisplayName','CO$_2$ - Ideal gas');
 hold off;
 xlabel('Pressure (bar)');
 ylabel('Density (kg/m$^3$)');
 title(['Fluid Densities at ', num2str(temperatureC), ' $^{\circ}$C']);
-legend('CH$_4$ - Peng-Robinson', 'CH$_4$ - Ideal Gas', 'CO$_2$ - Peng-Robinson', 'Location', 'East');
+set(findall(gcf, '-property', 'FontSize'), 'FontSize', 18);
+legend('Location', 'northwest');
 grid on;
